c48c177a6613ae91f816b9a031e5416c6565bb54
Riccardo Zanol <ricc@zanol.eu>
2016-05-25 15:47:14 +0200

    Change the transmitter_tf function

--- matlab/channel_response.m
+++ matlab/channel_response.m
@@ -1,6 +1,5 @@
-function [b, a, qc_length, qc] = transmitter_tf()
+function qc = channel_response(qc_length)
 b = [zeros(1,10), 0.7424];   %beta
 a = [1, -0.67];     %1-alfa
-qc_length = 20; % After this < 5e-5
 qc = impz(b, a, qc_length);
 end
