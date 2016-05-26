3919e471b25a91d13e4c55dd2a9bad6ebb133d65
Riccardo Zanol <ricc@zanol.eu>
2016-05-26 12:38:57 +0200

    Fix OFDM

--- matlab/ofdm_rx.m
+++ matlab/ofdm_rx.m
@@ -1,9 +1,24 @@
-function [y, Ty, y_subch, r_subch] = ofdm_rx(r, T_ofdm, M, Npx, gc)
+function [y, Ty, y_subch, T_dft, r_subch] = ofdm_rx(r, T_ofdm, M, Npx, gc, t0)
 % r: received signal, T_ofdm: period of the received signal, M: num of
 % subchannels, Npx: length of the cyclic prefix, gc: impulse response of
-% the channel + lowpass filters (g_rcos * q_c * g_rcos)
+% the channel + lowpass filters (g_rcos * q_c * g_rcos), t0: when to start
+% to demodulate (in multiples of T_ofdm)
 %
 % y: demodulated signal, Ty: period of the demodulated signal
+
+% Periods
+T_dft = T_ofdm * (Npx + M);
+Ty = T_dft / M;
+
+% Start to demodulate from t0
+r = r(t0+1:length(r));
+
+% Pad the input to multiples of M+Npx
+over_len = mod(length(r), M+Npx);
+if over_len ~= 0
+    pad_len = M+Npx - over_len;
+    r = [r; zeros(pad_len, 1)];
+end
 
 r_subch = reshape(r, M+Npx, []);
 x_subch_cyclic = r_subch(Npx+1:M+Npx,:); % Drop the cyclic prefix
@@ -14,7 +29,7 @@ if size(gc, 2) ~= 1
 end
 Gc = fft(gc, M);
 if any(abs(Gc) < 1e-2)
-    warning('Gc is very small');
+    warning('Gc is attenuating >=40dB');
 end
 y_subch = zeros(M, size(x_subch, 2));
 for k=1:size(x_subch, 2)
@@ -22,6 +37,4 @@ for k=1:size(x_subch, 2)
 end
 
 y = reshape(y_subch, [], 1);
-T_ofdm_no_prefix = T_ofdm * M / (M+Npx);
-Ty = T_ofdm_no_prefix * M;
 end
--- matlab/ofdm_tx.m
+++ matlab/ofdm_tx.m
@@ -1,8 +1,12 @@
-function [s, T_ofdm, a_subch, A_tx] = ofdm_tx(a, T, M, Npx)
+function [s, T_ofdm, a_subch, T_idft, A, A_tx] = ofdm_tx(a, T, M, Npx)
 % a: symbols, T: symbol period, M: num of subchannels, Npx: length of the
 % cyclic prefix
 %
 % s: modulated signal, T_ofdm: period of the modulated signal
+
+% Periods
+T_idft = M*T;
+T_ofdm = T_idft / (M+Npx);
 
 % Pad the input to multiples of M
 over_len = mod(length(a), M);
@@ -12,13 +16,8 @@ if over_len ~= 0
 end
 
 a_subch = reshape(a, M, []);
-%assert(all(a_subch(1,:).' == downsample(a, M, 0)));
 A = ifft(a_subch, [], 1);
-%assert(all(abs(A(1,:).' - ifft(downsample(a, M, 0))) < 1e-12));
 prefix = A(M-Npx+1:M,:);
 A_tx = [prefix; A];
 s = reshape(A_tx, [], 1);
-T_ofdm_no_prefix = T / M;
-T_ofdm = (M + Npx) / M * T_ofdm_no_prefix;
-%assert(all(s(Npx+1:M+Npx) == A(:,1)));
 end
