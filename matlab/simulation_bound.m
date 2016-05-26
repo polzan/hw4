68547d60dfaa92d4c46b942e544679bb614ee89d
Riccardo Zanol <ricc@zanol.eu>
2016-05-26 13:39:07 +0200

    Fix the simulation power

--- matlab/simulation_bound.m
+++ matlab/simulation_bound.m
@@ -1,10 +1,10 @@
 function [Pbit, err_count] = simulation_bound(Nbits, SNR, coded)
 [num_bits, tx_syms, tx_bits, uncoded_bits] = transmitter(Nbits, coded);
 
-sigma2 = sqrt(10^(-SNR/10));
+sigma = sqrt(10^(-SNR/10));
 rc_syms = awgn(tx_syms, SNR, 0);
 
-detected = receiver(rc_syms, length(uncoded_bits), sigma2, coded);
+detected = receiver(rc_syms, length(uncoded_bits), sigma^2, coded);
 
 err_count = sum(uncoded_bits ~= detected);
 Pbit = err_count / length(detected);
