e4085f7a6e314ea5b60de8f9b54d89645f0998d3
Riccardo Zanol <ricc@zanol.eu>
2016-05-27 15:24:40 +0200

    Update plots

--- matlab/build_all_plots.m
+++ matlab/build_all_plots.m
@@ -1,7 +1,7 @@
 close all; clear all; clc;
 
 % Uncoded
-SNRs = linspace(5, 15, 10);
+SNRs = linspace(3, 14, 10);
 Nbits = 5e5;
 
 % QAM lower bound
@@ -11,24 +11,28 @@ pe_theor_bound = 2 .* Q(sqrt(SNRs_lin));
 pbit_theor_bound = pe_theor_bound ./ 2;
 
 for i=1:length(SNRs)
-    pbit_awgn_u(i) = simulation_bound(Nbits, SNRs(i), 'uncoded');
+    pbit_awgn_u(i) = simulation_bound(Nbits, SNRs(i), 'coded');
+    pbit_ofdm_u(i) = simulate_ofdm(Nbits, SNRs(i), 'uncoded');
 end
 
 figure;
 semilogy(SNRs, pbit_theor_bound);
 hold on;
 semilogy(SNRs, pbit_awgn_u);
+semilogy(SNRs, pbit_ofdm_u);
 ylim([1e-5, 1e-1]);
 
 % Coded
-SNRs = linspace(1.5, 3.5, 10);
-Nbits = 1e6;
+SNRs = linspace(1.5, 3.5, 5);
+Nbits = 1e5;
 
 for i=1:length(SNRs)
     pbit_awgn_c(i) = simulation_bound(Nbits, SNRs(i), 'coded');
+    pbit_ofdm_c(i) = simulate_ofdm(Nbits, SNRs(i), 'coded');
 end
 
 figure;
 semilogy(SNRs, pbit_awgn_c);
 hold on;
+semilogy(SNRs, pbit_ofdm_c);
 ylim([1e-5, 1e-1]);
