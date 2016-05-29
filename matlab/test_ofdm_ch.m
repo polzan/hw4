7d86b0b0ea96faf34bc6edb6d2e0d4e3b491001b
Riccardo Zanol <ricc@zanol.eu>
2016-05-29 18:04:24 +0200

    Update the test for OFDM

--- matlab/test_ofdm_ch.m
+++ matlab/test_ofdm_ch.m
@@ -1,26 +1,42 @@
 close all; %clear all; clc;
-Nbits = 2^18;
-[n_info_bits, a, bits, uncoded_bits] = transmitter(Nbits, 'uncoded');
-T = 1;
+Nbits = 1e6;
+coded = 'uncoded';
+
+skip_final_transient = 100;
+Nbits = Nbits + skip_final_transient;
+
+[n_info_bits, a, bits, uncoded_bits] = transmitter(Nbits, coded);
+T = 1; % symbol period
 
 M = 512;
-Npx = 0; % length of gc - 1
+Npx = 16; % length of gc - 1
 Nvirt = 0;
+
+% OFDM modulation
 [s, T_ofdm, a_subch] = ofdm_tx(a, T, M, Npx, Nvirt);
 
-SNR = 11;
-t0 = 34; % 2 half-length of rcos + peak of qc
+% Channel
+SNR = 14;
+t0 = 35; % 2 half-length of rcos + peak of qc
 qc_length = 20;
 rcos_length = 25;
 [r, gc, T_c, t0_sampled, sigma2_w] = ofdm_channel(s, T_ofdm, SNR, t0, qc_length, rcos_length, M, Npx, Nvirt);
-
 
 figure;
 stem((0:length(gc)-1), gc);
 hold on;
 plot(t0_sampled .* ones(2,1), ylim);
 
+% OFDM demodulator
 [y, Ty, y_subch] = ofdm_rx(r, T_ofdm, M, Npx, Nvirt, gc, t0_sampled);
+
+% Decode symbols
+sigma2_W = (M-Nvirt)*sigma2_w;
+bits_det = receiver(y, n_info_bits, sigma2_W, coded);
+
+% Check errors
+err_count = sum(uncoded_bits(1:length(uncoded_bits)-skip_final_transient) ~= bits_det(1:length(bits_det)-skip_final_transient));
+Pbit = err_count/(length(uncoded_bits)-skip_final_transient)
 
 % figure;
 % subplot(2,1,1);
@@ -43,23 +59,3 @@ end
 figure;
 plot(0:M-1-Nvirt, Pbits);
 title('Pbit per subch');
-
-% figure;
-% scatter(real(y), imag(y));
-% hold on;
-% scatter(real(a), imag(a), 'rx');
-
-bits_det = receiver(y, n_info_bits, (M-Nvirt)*sigma2_w, 'uncoded');
-
-%bits_cut = bits(1:length(bits_det));
-
-err = sum(uncoded_bits ~= bits_det);
-Pbit = err/length(bits_det)
-
-
-
-
-% figure;
-% stem((0:length(bits)-1), bits .* 0.9);
-% hold on;
-% stem((0:length(bits_det)-1) + t0_sampled, bits_det);
