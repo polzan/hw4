a5e3b9bf9981c3eeff064a1a5847372bac0de447
Riccardo Zanol <ricc@zanol.eu>
2016-05-29 17:52:07 +0200

    Progress indication

--- matlab/run_coded.m
+++ matlab/run_coded.m
@@ -7,6 +7,15 @@ for i=1:length(SNRs)
     pbit_awgn_c(i) = simulation_bound(Nbits, SNRs(i), 'coded');
     pbit_ofdm_c(i) = simulate_ofdm(Nbits, SNRs(i), 'coded');
     pbit_dfe_c(i) = simulate_dfe(Nbits, SNRs(i), 'coded');
+    
+    if mod(i, 5) == 0
+        fprintf('%d', i);
+    else
+        fprintf('.');
+    end
 end
+fprintf('\n');
 
 save('pbits_coded.mat');
+
+fprintf('Coded OK\n');
--- matlab/run_uncoded.m
+++ matlab/run_uncoded.m
@@ -7,6 +7,15 @@ for i=1:length(SNRs)
     pbit_awgn_u(i) = simulation_bound(Nbits, SNRs(i), 'uncoded');
     pbit_ofdm_u(i) = simulate_ofdm(Nbits, SNRs(i), 'uncoded');
     pbit_dfe_u(i) = simulate_dfe(Nbits, SNRs(i), 'uncoded');
+    
+    if mod(i, 5) == 0
+        fprintf('%d', i);
+    else
+        fprintf('.');
+    end
 end
+fprintf('\n');
 
 save('pbits_uncoded.mat');
+
+fprintf('Uncoded OK\n');
