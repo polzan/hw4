823200d9d47ac70afbbc7321fee9ecd4a0e5b7ce
Riccardo Zanol <ricc@zanol.eu>
2016-05-31 12:40:34 +0200

    Edit the OFDM params

--- matlab/global_parameters.m
+++ matlab/global_parameters.m
@@ -1,13 +1,13 @@
 function params = global_parameters()
 params.sym_period = 1;
 
-params.channel.qc_length = 25;
+params.channel.qc_length = 20;
 
 params.ofdm.M = 512;
-params.ofdm.Npx = 24;
-params.ofdm.Nvirt = 91;
+params.ofdm.Npx = 12;
+params.ofdm.Nvirt = 48;
 
-params.ofdm.t0 = 51;
+params.ofdm.t0 = 27;
 params.ofdm.rcos_length = 40;
 
 params.dfe.M1 = 3;
