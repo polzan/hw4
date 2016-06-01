7bda27787411d41f01ac63f55ee19397e4d5c1a6
Riccardo Zanol <ricc@zanol.eu>
2016-06-01 08:11:29 +0200

    Revert "Change the t0 and the Npx"
    This reverts commit 2b5871a7f8b6cdf471153b469b6a2dbd4dfa49c9.

--- matlab/global_parameters.m
+++ matlab/global_parameters.m
@@ -4,10 +4,10 @@ params.sym_period = 1;
 params.channel.qc_length = 20;
 
 params.ofdm.M = 512;
-params.ofdm.Npx = 10;
+params.ofdm.Npx = 12;
 params.ofdm.Nvirt = 48;
 
-params.ofdm.t0 = 31;
+params.ofdm.t0 = 27;
 params.ofdm.rcos_length = 40;
 
 params.dfe.M1 = 3;
--- matlab/ofdm_plots.m
+++ matlab/ofdm_plots.m
@@ -48,7 +48,7 @@ figure;
 stem((0:length(gc)-1)-t0_sampled, gc);
 xlabel('m [@T_{ofdm}]');
 ylabel('h(mT_{ofdm})');
-xlim([-3 13]);
+xlim([-2 15]);
 grid on;
 print('h', '-deps');
 
